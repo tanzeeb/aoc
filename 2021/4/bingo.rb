6cf12b5dbd3d7c87b185fdef593e401f40f83c15
Tanzeeb Khalili <tanzeeb@gmail.com>
2021-12-24 22:25:21 -0500

    Day 4, part 1

---
+++ 2021/4/bingo.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+numbers = ARGF.readline.chomp.split(',')
+
+cards = ARGF.readlines.each_slice(6).map { |lines| lines[1..].map &:split }
+
+numbers.each do |number|
+  cards.map! { |card| card.map! { |row| row.map! { |entry| entry == number ? "-" : entry } } }
+
+  winners = cards.select do |card|
+    card.any? { |row| row.all? { |entry| entry == "-" } } ||
+      (0..card.first.length).any? { |index| card.all? { |row| row[index] == "-" } }
+  end
+
+  if winners.length > 0
+    puts winners.first.flatten.map(&:to_i).sum * number.to_i
+
+    break
+  end
+end
+
