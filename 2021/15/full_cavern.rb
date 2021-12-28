344147f00d5d6296a176781da9be9bf972daa0a8
Tanzeeb Khalili <tanzeeb@gmail.com>
2021-12-28 11:17:21 -0500

    Day 15, part 2 wip

---
+++ 2021/15/full_cavern.rb
@@ -0,0 +1,70 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'set'
+
+tile = ARGF.readlines(chomp: true).map { |line| line.split("").map(&:to_i) }
+
+rows = tile.size
+cols = tile.first.size
+
+map = (0...rows*5).map do |x|
+  (0...cols*5).map do |y|
+    (tile[x%cols][y%rows] + x/rows + y/cols - 1) % 9 + 1
+  end
+end
+
+max_x = map.first.size - 1
+max_y = map.size - 1
+
+source = [0,0]
+target = [max_x, max_y]
+
+q = Set[]
+dist = {}
+prev = {}
+
+(0..max_x).each do |x|
+  (0..max_y).each do |y|
+    v = [x,y]
+    dist[v] = Float::INFINITY
+    prev[v] = nil
+    q << v
+  end
+end
+
+dist[source] = 0
+qdist = dist.dup
+
+until q.empty?
+  u = qdist.min_by{|_,v| v }.first
+
+  q.delete u
+  qdist.delete u
+
+  break if u == target
+
+  ux, uy = u
+  neighbours = []
+
+  neighbours << [ux+1,uy] if ux < max_x
+  neighbours << [ux-1,uy] if ux > 0
+  neighbours << [ux,uy+1] if uy < max_y
+  neighbours << [ux,uy-1] if uy > 0
+
+  neighbours.select! { |v| q.include? v }
+
+  neighbours.each do |v|
+    vx, vy = v
+
+    alt = dist[u] + map[vx][vy]
+
+    if alt < dist[v]
+      dist[v] = alt
+      qdist[v] = alt
+      prev[v] = u
+    end
+  end
+end
+
+puts dist[target]
