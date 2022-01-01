b2b10ab24055a26b4da01aae69b875614c4e4fa1
Tanzeeb Khalili <tanzeeb@gmail.com>
2021-12-31 20:43:14 -0500

    Day 21, part 1

---
+++ 2021/21/dice.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+pawns = ARGF.readlines(chomp: true).map { |line| line.split(': ').last.to_i }
+dice = 100
+rolls = 0
+
+scores = [0,0]
+
+def move start, spaces
+  (start+spaces-1) % 10 + 1
+end
+
+loop do
+  pawns.size.times do |i|
+    spaces = 3.times.map { dice = dice % 100 + 1 }.sum
+    rolls += 3
+
+    pawns[i] = (pawns[i]+spaces-1) % 10 + 1
+
+    scores[i] += pawns[i]
+
+    if scores[i] >= 1000
+      puts scores.min * rolls
+
+      exit
+    end
+  end
+end
+
