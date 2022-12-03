1ec312760e217ed6b71e0831136383349cf35084
Tanzeeb Khalili <tanzeeb@gmail.com>
2022-12-03 00:28:35 -0500

    Day 3, part 2

--- 2022/3/b.rb
+++ 2022/3/b.rb
@@ -1,7 +1,21 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-ARGF.readlines(chomp: true).each do |line|
-  puts line
+priorities = 0
+
+ARGF.readlines(chomp: true).each_slice(3) do |sacks|
+  first, second, third = sacks.map {|sack| sack.split(//)}
+
+  common, _ = first & second & third
+
+  priority = case common
+             when "a".."z"
+               common.ord - "a".ord + 1
+             when "A".."Z"
+               common.ord - "A".ord + 27
+             end
+
+  priorities += priority
 end
 
+puts priorities
