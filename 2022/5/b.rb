c1a16e648434091ffc07b215a039725e5419fffb
Tanzeeb Khalili <tanzeeb@gmail.com>
2022-12-05 00:53:33 -0500

    Day 5, part 2

--- 2022/5/b.rb
+++ 2022/5/b.rb
@@ -1,7 +1,27 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+moves = []
+stacks = Hash.new { |h,k| h[k] = [] }
+
 ARGF.readlines(chomp: true).each do |line|
-  puts line
+  case line
+
+  when /^move/
+    count, from, to = line.scan(/\d+/).map(&:to_i)
+    moves << [ count, from, to ]
+
+  when /\[[A-Z]\]/
+    line.split(//).each_slice(4).with_index do |(_,crate,_,_),i|
+      stacks[i+1].prepend crate unless crate.strip.empty?
+    end
+
+  end
 end
 
+moves.each do |(count, from, to)|
+  crates = stacks[from].pop(count)
+  stacks[to].push *crates
+end
+
+puts stacks.keys.sort.map { |k| stacks[k].last }.join("")
