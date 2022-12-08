fc363c1bf87f3a2684473f57cec79495d3086cd2
Tanzeeb Khalili <tanzeeb@gmail.com>
2022-12-08 01:09:45 -0500

    Day 8, part 2

--- 2022/8/b.rb
+++ 2022/8/b.rb
@@ -1,7 +1,47 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-ARGF.readlines(chomp: true).each do |line|
-  puts line
+MAP = ARGF.readlines(chomp: true).map { |line| line.split(//).map(&:to_i) }
+
+ROWS = MAP.size
+COLS = MAP.first.size
+
+def score row, col
+  height = MAP[row][col]
+
+  up = 0
+  (row-1).downto(0).each do |i|
+    up += 1
+    break unless height > MAP[i][col]
+  end
+
+  down = 0
+  (row+1).upto(ROWS-1).each do |i|
+    down += 1
+    break unless height > MAP[i][col]
+  end
+
+  left = 0
+  (col-1).downto(0).each do |j|
+    left += 1
+    break unless height > MAP[row][j]
+  end
+
+  right = 0
+  (col+1).upto(COLS-1).each do |j|
+    right += 1
+    break unless height > MAP[row][j]
+  end
+
+  up * down * left * right
 end
 
+max = 0
+scores = (0...ROWS).map do |row|
+  (0...COLS).map do |col|
+    s = score(row,col)
+    max = s if s > max
+  end
+end
+
+puts max
