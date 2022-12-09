274d9b1449fbe4e402d29889a79bbce1e4edce9f
Tanzeeb Khalili <tanzeeb@gmail.com>
2022-12-09 14:39:31 -0500

    Day 8, part 2

--- 2022/9/b.rb
+++ 2022/9/b.rb
@@ -9,61 +9,76 @@ history = Set.new
 
 history << knots.last.to_s
 
-pp knots
+def delta a, b
+  return 0 if a == b
+
+  a > b ? -1 : 1
+end
+
+def update! knots
+  9.times do |i|
+    if knots[i][1] - knots[i+1][1] > 1
+      knots[i+1][1] = knots[i][1] - 1
+      knots[i+1][0] += delta knots[i+1][0], knots[i][0]
+    end
+    if knots[i][1] - knots[i+1][1] < -1
+      knots[i+1][1] = knots[i][1] + 1
+      knots[i+1][0] += delta knots[i+1][0], knots[i][0]
+    end
+    if knots[i][0] - knots[i+1][0] > 1
+      knots[i+1][0] = knots[i][0] - 1
+      knots[i+1][1] += delta knots[i+1][1], knots[i][1]
+    end
+    if knots[i][0] - knots[i+1][0] < -1
+      knots[i+1][0] = knots[i][0] + 1
+      knots[i+1][1] += delta knots[i+1][1], knots[i][1]
+    end
+  end
+end
+
+def draw knots
+  #min_x,max_x = knots.map { |k| k[0] }.minmax
+  #min_y,max_y = knots.map { |k| k[1] }.minmax
+
+  chart = Array.new(30) { Array.new(30,".") }
+
+  knots.each.with_index do |k,i|
+    chart[15-k[1]][15+k[0]] = i.to_s
+  end
+
+  chart.each do |line|
+    puts line.join("")
+  end
+  puts ""
+end
 
 ARGF.readlines(chomp: true).each do |line|
-  puts line
-
   case line
   when /^U (\d+)$/
     $1.to_i.times do 
       knots[0][1] += 1
-      9.times do |i|
-        if knots[i][1] - knots[i+1][1] > 1
-          knots[i+1][1] = knots[i][1] - 1
-          knots[i+1][0] = knots[i][0]
-        end
-      pp knots
-      end
+      update! knots
       history << knots.last.to_s
     end
   when /^D (\d+)$/
     $1.to_i.times do 
       knots[0][1] -= 1
-      9.times do |i|
-        if knots[i][1] - knots[i+1][1] < -1
-          knots[i+1][1] = knots[i][1] + 1
-          knots[i+1][0] = knots[i][0]
-        end
-      end
+      update! knots
       history << knots.last.to_s
     end
   when /^R (\d+)$/
     $1.to_i.times do 
       knots[0][0] += 1
-      9.times do |i|
-        if knots[i][0] - knots[i+1][0] > 1
-          knots[i+1][0] = knots[i][0] - 1
-          knots[i+1][1] = knots[i][1]
-        end
-      end
+      update! knots
       history << knots.last.to_s
     end
   when /^L (\d+)$/
     $1.to_i.times do 
       knots[0][0] -= 1
-      9.times do |i|
-        if knots[i][0] - knots[i+1][0] < -1
-          knots[i+1][0] = knots[i][0] + 1
-          knots[i+1][1] = knots[i][1]
-        end
-      end
+      update! knots
       history << knots.last.to_s
     end
   end
-  pp knots
 end
-
-pp history
 
 puts history.size
