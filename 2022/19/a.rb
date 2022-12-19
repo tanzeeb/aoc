1b88226a61631e53ad3b645d8347d057c8f9ebaf
Tanzeeb Khalili <tanzeeb@gmail.com>
2022-12-19 02:30:10 -0500

    Day 19, wip

--- 2022/19/a.rb
+++ 2022/19/a.rb
@@ -1,14 +1,68 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-Blueprint = Struct.new(:id, :ore, :clay, :obsidian, :geode)
-Ore = Struct.new(:ore)
-Clay = Struct.new(:ore)
-Obsidian = Struct.new(:ore, :clay)
-Geode = Struct.new(:ore, :obsidian)
+require 'set'
 
-Inventory = Struct.new(:ore, :clay, :obsidian, :geode)
-Robots = Struct.new(:ore, :clay, :obsidian, :geode)
+TYPES = [:ore, :clay, :obsidian, :geode]
+
+Blueprint = Struct.new(:id, :ore, :clay, :obsidian, :geode) do
+  def builds robots, inventory
+    bs = Set[]
+
+    queue = [[robots.dup, inventory.dup]]
+
+    while build = queue.pop
+      bs << build
+      r,i = build
+
+      TYPES.each do |type|
+        count = self[type].how_many? i
+
+        count.times do |n|
+          n = [r.dup.add!(type, n+1), i.dup.deduct!(self[type], n+1)]
+
+          queue << n unless bs.include? n
+        end
+      end
+    end
+
+    bs
+  end
+end
+
+Cost = Struct.new(*TYPES) do
+  def how_many? inventory
+    TYPES.select {|t| self[t] > 0 }.map do |type|
+      inventory[type] / self[type]
+    end.min
+  end
+end
+
+Inventory = Struct.new(*TYPES) do
+  def collect! robots
+    TYPES.each do |type|
+      self[type] += robots[type]
+    end
+
+    self
+  end
+
+  def deduct! cost, count
+    TYPES.each do |type|
+      self[type] -= cost[type] * count
+    end
+
+    self
+  end
+end
+
+Robots = Struct.new(*TYPES) do
+  def add! type, count
+    self[type] += count
+
+    self
+  end
+end
 
 blueprints = []
 
@@ -20,38 +74,29 @@ ARGF.readlines(chomp: true).each do |line|
     when /Blueprint (\d+)/
       blueprint.id = $1.to_i
     when /Each ore robot costs (\d+) ore/
-      blueprint.ore = Ore.new($1.to_i)
+      blueprint.ore = Cost.new($1.to_i,0,0,0)
     when /Each clay robot costs (\d+) ore/
-      blueprint.clay = Clay.new($1.to_i)
+      blueprint.clay = Cost.new($1.to_i,0,0,0)
     when /Each obsidian robot costs (\d+) ore and (\d+) clay/
-      blueprint.obsidian = Obsidian.new($1.to_i, $2.to_i)
+      blueprint.obsidian = Cost.new($1.to_i, 0, $2.to_i, 0)
     when /Each geode robot costs (\d+) ore and (\d+) obsidian/
-      blueprint.geode = Geode.new($1.to_i, $2.to_i)
-    else
+      blueprint.geode = Cost.new($1.to_i, 0, 0, $2.to_i,)
     end
   end
 
   blueprints << blueprint
 end
 
-def collect! robots, inventory
-  inventory.ore += robots.ore
-  inventory.clay += robots.clay
-  inventory.obsidian += robots.obsidian
-  inventory.geode += robots.geode
-end
-
-def builds blueprint, robots, inventory
-
-end
-
 def geodes blueprint, robots, inventory, minutes
-  return inventory.geodes if minutes < 1
+  return inventory.geode if minutes < 1
 
-  collect! robots, inventory
+  inventory.collect! robots
 
-  options = builds blueprint, robots, inventory
+  options = blueprint.builds robots, inventory
 
   options.map {|(r,i)| geodes(blueprint,r,i,minutes-1)}.max
 end
 
+pp geodes(blueprints.first, Robots.new(1,0,0,0), Inventory.new(0,0,0,0), 24)
+
+#puts blueprints.map {|b| geodes(b, Robots.new(0,0,0,0), Inventory.new(0,0,0,0), 24) * b.id}.sum
