47dd3cb504d9581eee146abae4837eb6e0a56687
Tanzeeb Khalili <tanzeeb@gmail.com>
2022-12-20 02:23:57 -0500

    Day 20, part 2

--- 2022/20/b.rb
+++ 2022/20/b.rb
@@ -1,7 +1,70 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-ARGF.readlines(chomp: true).each do |line|
-  puts line
+Node = Struct.new(:v,:p,:n) do
+  def move count
+    return if count == 0
+
+    self.p.n = self.n
+    self.n.p = self.p
+
+    if count > 0
+      n = self.n
+      count.times { n = n.n }
+
+      self.p = n.p
+      self.p.n = self
+
+      n.p = self
+      self.n = n
+    end
+
+    if count < 0
+      p = self.p
+      count.abs.times { p = p.p }
+
+      self.n = p.n
+      self.n.p = self
+
+      p.n = self
+      self.p = p
+    end
+  end
+
+  def to_s
+    { value: self.v, prev: self.p.v, next: self.n.v }.to_s
+  end
+
+  def to_a
+    list = [self.v]
+
+    current = self
+    list << current.v until (current = current.n) == self
+
+    list
+  end
 end
 
+KEY = 811589153
+
+file = ARGF.readlines(chomp: true).map { |n| Node.new(n.to_i*KEY,nil,nil) }
+
+file.first.p = file.last
+file.last.n = file.first
+
+file.each_cons(2) do |p,n|
+  p.n = n
+  n.p = p
+end
+
+def mix file
+  file.each do |node|
+    node.move(node.v % (file.size-1))
+  end
+end
+
+10.times { mix file }
+
+zero = file.find {|n| n.v == 0}.to_a
+
+puts [1000,2000,3000].map {|n| zero[n%zero.size] }.sum
