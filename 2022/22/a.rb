9fd25e47a4dcb50d07588aa71ad02ee90cbcb591
Tanzeeb Khalili <tanzeeb@gmail.com>
2022-12-25 04:56:24 -0500

    Day 22, wip

--- 2022/22/a.rb
+++ 2022/22/a.rb
@@ -5,12 +5,35 @@ puts "---"
 
 *map, _, path = ARGF.readlines(chomp: true).map {|l| l.split(//)}
 
-pp map
-pp path
-
 nodes = {}
 
 Node = Struct.new(:x,:y,:up,:down,:left,:right) do
+  def to_s
+    { x: x, y: y }.to_s
+  end
+
+  def inspect
+    { x: x, y: y }.inspect
+  end
+end
+
+Position = Struct.new(:node, :facing) do
+  def rotate direction
+    case direction
+    when "R"
+      self.facing = (self.facing+1) % 4
+    when "L"
+      self.facing = (self.facing-1) % 4
+    end
+  end
+
+  def move count
+    @directions ||= [:right, :down, :left, :up]
+
+    count.times do
+      self.node = self.node.send @directions[self.facing]
+    end
+  end
 end
 
 start = nil
@@ -27,7 +50,12 @@ map.each.with_index do |line,y|
   line.each.with_index do |tile,x|
     next if tile == " "
 
-    nodes[[x,y]] = Node.new(x,y) if tile == "."
+    if tile == "."
+      node = Node.new(x,y)
+
+      nodes[[x,y]] = node
+      start ||= node
+    end
 
     ups[x] = y if y < ups[x]
     downs[x] = y if y > downs[x]
@@ -37,19 +65,16 @@ map.each.with_index do |line,y|
   end
 end
 
-pp rows
-pp cols
-
-pp ups
-pp downs
-pp lefts
-pp rights
-
 nodes.each do |(x,y),node|
   left = [ x-1 < lefts[y] ? rights[y] : x-1, y]
   right = [ x+1 > rights[y] ? lefts[y] : x+1, y]
   up = [x, y-1 < ups[x] ? downs[x] : y-1]
   down = [x, y+1 > downs[x] ? ups[x] : y+1 ]
+
+  node.left = node
+  node.right = node
+  node.up = node
+  node.down = node
 
   node.left = nodes[left] unless map[left.last][left.first] == "#"
   node.right = nodes[right] unless map[right.last][right.first] == "#"
@@ -62,10 +87,10 @@ def draw nodes, rows, cols
     cols.times.map { |x|
       if node = nodes[[x,y]]
         mask = 0
-        mask += 1 if node.up
-        mask += 2 if node.right
-        mask += 4 if node.down
-        mask += 8 if node.left
+        mask += 1 unless node.up == node
+        mask += 2 unless node.right == node
+        mask += 4 unless node.down == node
+        mask += 8 unless node.left == node
 
         mask.to_s(16)
       else
@@ -75,4 +100,11 @@ def draw nodes, rows, cols
   }.join("\n")
 end
 
-draw nodes, rows, cols
+position = Position.new(start, 0)
+
+path.join("").scan(/(\d+)([RL])/) do |(moves,turn)|
+  position.move(moves.to_i)
+  position.rotate(turn)
+end
+
+puts [(position.node.y+1)*1000, (position.node.x+1)*4, position.facing].sum
