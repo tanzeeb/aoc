c78deea81cdedc53b93c6b3a278c95feee743922
Tanzeeb Khalili <tanzeeb@gmail.com>
2023-12-01 05:10:53 -0500

    Day 1, part 2

--- 2023/1/b.rb
+++ 2023/1/b.rb
@@ -1,7 +1,20 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-ARGF.readlines(chomp: true).each do |line|
-  puts line
-end
+DIGITS = {
+    "one" => "1", "two" => "2", "three" => "3",
+    "four" => "4", "five" => "5", "six" => "6",
+    "seven" => "7", "eight" => "8", "nine" => "9",
+
+    "1" => "1", "2" => "2", "3" => "3",
+    "4" => "4", "5" => "5", "6" => "6",
+    "7" => "7", "8" => "8", "9" => "9",
+  }
+
+puts ARGF.readlines(chomp: true).sum { |line| 
+  [
+    DIGITS.keys.min_by {|d| line.index(d) || Float::INFINITY },
+    DIGITS.keys.max_by {|d| line.rindex(d) || -1 }
+  ].map {|d| DIGITS[d]}.compact.join.to_i
+}
 
