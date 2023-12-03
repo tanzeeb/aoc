4a71de38c40e63809fb05b2256a7b812abbbcf73
Tanzeeb Khalili <tanzeeb@gmail.com>
2023-12-03 00:14:17 -0500

    Day 2, part 1

--- 2023/2/a.rb
+++ 2023/2/a.rb
@@ -1,7 +1,15 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-ARGF.readlines(chomp: true).each do |line|
-  puts line
+FILTER = { "red" => 12, "green" => 13, "blue" => 14 }
+
+games = ARGF.readlines(chomp: true).map do |game|
+  {}.tap do |max|
+    game.scan(/(\d+) ([a-z]+)/).each do |(count,colour)| 
+      max[colour] = count.to_i if max[colour].to_i < count.to_i
+    end
+  end
 end
+
+puts games.map.with_index { |g,i| FILTER.all? { |(k,v)| g[k] <= v } ? i+1 : 0 }.sum
 
