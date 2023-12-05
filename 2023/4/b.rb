6f9146e575a74c80d3cc0551e8c14b794e2bfab6
Tanzeeb Khalili <tanzeeb@gmail.com>
2023-12-05 01:13:05 -0500

    Day 4, part 2

--- 2023/4/b.rb
+++ 2023/4/b.rb
@@ -1,7 +1,21 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+require 'debug'
+
+counts = Hash.new(0)
+
 ARGF.readlines(chomp: true).each do |line|
-  puts line
+  l,r = line.split(/:/)
+  c = l.split(/\W+/).last.to_i
+  w,n = r.split(/\|/).map{|p| p.strip.split(/\W+/).map(&:to_i)}
+  m = (w & n).size
+
+  counts[c] += 1
+
+  m.times do |i|
+    counts[c+i+1] += counts[c]
+  end
 end
 
+puts counts.values.sum
