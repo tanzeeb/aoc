45e2c111ca95d992b0a57a4c70f7c3588f65eabb
Tanzeeb Khalili <tanzeeb@gmail.com>
2023-12-07 11:14:38 -0500

    Day 6, part 1

--- 2023/6/a.rb
+++ 2023/6/a.rb
@@ -1,7 +1,17 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-ARGF.readlines(chomp: true).each do |line|
-  puts line
-end
+file = ARGF.readlines(chomp: true)
+
+times = file.first.split(":").last.strip.split(/\W+/).map(&:to_i)
+distances = file.last.split(":").last.strip.split(/\W+/).map(&:to_i)
+
+races = times.zip(distances)
+
+pp races.map { |(t,d)|
+  x1 = (t - Math.sqrt(t*t - 4*d))/2.0 
+  x2 = (t + Math.sqrt(t*t - 4*d))/2.0 
+
+  ( (x1.ceil==x1 ? x1.ceil+1 : x1.ceil) .. (x2.floor==x2 ? x2.floor-1 : x2.floor) ).size
+}.inject(&:*)
 
