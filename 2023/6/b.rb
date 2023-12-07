593dae0b625249b8a24d4d525ac23257abdf6ce3
Tanzeeb Khalili <tanzeeb@gmail.com>
2023-12-07 11:20:32 -0500

    Day 6, part 2

--- 2023/6/b.rb
+++ 2023/6/b.rb
@@ -1,7 +1,13 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-ARGF.readlines(chomp: true).each do |line|
-  puts line
-end
+file = ARGF.readlines(chomp: true)
+
+t = file.first.split(":").last.gsub(" ","").to_i
+d = file.last.split(":").last.gsub(" ","").to_i
+
+x1 = (t - Math.sqrt(t*t - 4*d))/2.0 
+x2 = (t + Math.sqrt(t*t - 4*d))/2.0 
+
+pp ( (x1.ceil==x1 ? x1.ceil+1 : x1.ceil) .. (x2.floor==x2 ? x2.floor-1 : x2.floor) ).size
 
