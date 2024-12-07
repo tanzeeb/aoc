ea34d6b633fcf1bbcfaa965c1fecf11a05144a15
Tanzeeb Khalili <tanzeeb@gmail.com>
2024-12-07 01:24:47 -0500

    day 2, b

--- 2024/02/b.rb
+++ 2024/02/b.rb
@@ -1,7 +1,21 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-ARGF.readlines(chomp: true).each do |line|
-  puts line
+safe = 0
+
+def safe?(levels)
+  levels.reverse! if levels.first > levels.last
+
+  levels.each_cons(2).map { |a, b| b - a }.all? { |d| d >= 1 && d <= 3 }
 end
 
+ARGF.readlines(chomp: true).each do |report|
+  levels = report.split(/ /).map(&:to_i)
+
+  perms = [levels]
+  perms += levels.combination(levels.size - 1).to_a
+
+  safe += 1 if perms.any? { |l| safe? l }
+end
+
+puts safe
