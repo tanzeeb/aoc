032b939ec08ef0c0e82bdfe593c4dcddb31b7830
Tanzeeb Khalili <tanzeeb@gmail.com>
2024-12-07 17:32:38 -0500

    day 6, a

---
+++ 2024/06/a.rb
@@ -0,0 +1,83 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+@map = ARGF.readlines(chomp: true).map { |line| line.split(//) }
+
+@rows = @map.size
+@cols = @map.first.size
+
+@obstacles = Hash.new(false)
+@path = Hash.new(false)
+@pos = nil
+@dir = [-1, 0]
+
+@rows.times do |row|
+  @cols.times do |col|
+    case @map[row][col]
+    when '#'
+      @obstacles[[row, col]] = true
+    when '^'
+      @pos = [row, col]
+    end
+  end
+end
+
+def draw
+  @rows.times do |row|
+    @cols.times do |col|
+      print symbol(row, col)
+    end
+    puts
+  end
+  puts
+end
+
+def symbol(row, col)
+  if @obstacles[[row, col]]
+    '#'
+  elsif @pos.first == row && @pos.last == col
+    case @dir
+    when [-1, 0]
+      '^'
+    when [1, 0]
+      'v'
+    when [0, 1]
+      '>'
+    when [0, -1]
+      '<'
+    end
+  elsif @path[[row, col]]
+    'X'
+  else
+    '.'
+  end
+end
+
+def turn!
+  @dir = case @dir
+         when [-1, 0]
+           [0, 1]
+         when [0, 1]
+           [1, 0]
+         when [1, 0]
+           [0, -1]
+         when [0, -1]
+           [-1, 0]
+         end
+end
+
+def walk!
+  forward = [@pos.first + @dir.first, @pos.last + @dir.last]
+
+  if @obstacles[forward]
+    turn!
+  else
+    @path[@pos] = true
+
+    @pos = forward
+  end
+end
+
+walk! while @pos.first.positive? && @pos.first < @rows && @pos.last.positive? && @pos.last < @cols
+
+pp(@path.count { |_, v| v })
