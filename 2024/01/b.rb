fc4a366088e4ad646576a0309aa9dbe3537dec9d
Tanzeeb Khalili <tanzeeb@gmail.com>
2024-12-07 00:43:02 -0500

    day 1, b

--- 2024/01/b.rb
+++ 2024/01/b.rb
@@ -1,7 +1,19 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+left = []
+right = Hash.new(0)
+sim = []
+
 ARGF.readlines(chomp: true).each do |line|
-  puts line
+  l, r = line.split(/\W+/).map(&:to_i)
+
+  left << l
+  right[r] += 1
 end
 
+left.each do |l|
+  sim << l * right[l]
+end
+
+puts sim.sum
