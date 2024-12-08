0e2af4e4cd9553f5169b050f7c1b63655192e9e0
Tanzeeb Khalili <tanzeeb@gmail.com>
2024-12-07 20:47:55 -0500

    day 7, b

--- 2024/07/b.rb
+++ 2024/07/b.rb
@@ -1,7 +1,18 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-ARGF.readlines(chomp: true).each do |line|
-  puts line
+@equations = ARGF.readlines(chomp: true).map do |e|
+  r, expr = e.split(/:\W+/)
+
+  [r.to_i, expr.split(/\W+/).map(&:to_i)]
 end
 
+def match?(left, right, rest)
+  return left == right if rest.empty?
+
+  match?(left, right * rest.first, rest[1..]) ||
+    match?(left, right + rest.first, rest[1..]) ||
+    match?(left, "#{right}#{rest.first}".to_i, rest[1..])
+end
+
+puts(@equations.filter { |(r, op)| match? r, op.first, op[1..] }.sum { |(r, _)| r })
