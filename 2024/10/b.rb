b1583a1d4097ab2aac8b30f0b5fe8c9bf4e32cc9
Tanzeeb Khalili <tanzeeb@gmail.com>
2024-12-10 08:10:34 -0500

    Day 10, b

--- 2024/10/b.rb
+++ 2024/10/b.rb
@@ -1,7 +1,41 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-ARGF.readlines(chomp: true).each do |line|
-  puts line
+@map = ARGF.readlines(chomp: true).map { |l| l.split(//).map(&:to_i) }
+
+@rows = @map.size
+@cols = @map.first.size
+
+@scores = 0
+
+@map.each_with_index do |row, i|
+  row.each_with_index do |start, j|
+    score = 0
+    stack = []
+
+    stack.push [i, j] if start.zero?
+
+    until stack.empty?
+      pos = stack.pop
+      height = @map[pos.first][pos.last]
+
+      if height == 9
+        score += 1
+      else
+        [[1, 0], [-1, 0], [0, 1], [0, -1]].each do |dir|
+          adj = [pos.first + dir.first, pos.last + dir.last]
+
+          next if adj.first.negative? || adj.first >= @rows || adj.last.negative? || adj.last >= @cols
+
+          next unless @map[adj.first][adj.last] == height + 1
+
+          stack.push adj
+        end
+      end
+    end
+
+    @scores += score
+  end
 end
 
+puts @scores
