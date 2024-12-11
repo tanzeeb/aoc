6096f81330a01755788948eac7721fe46fdbf302
Tanzeeb Khalili <tanzeeb@gmail.com>
2024-12-11 00:45:29 -0500

    Day 11, b

--- 2024/11/b.rb
+++ 2024/11/b.rb
@@ -1,7 +1,25 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-ARGF.readlines(chomp: true).each do |line|
-  puts line
+@stones = ARGF.readline(chomp: true).split(/ /).map(&:to_i)
+
+@count = Hash.new do |h, k|
+  stone = k.first
+  count = k.last
+
+  mark = stone.to_s
+  left = mark[0...mark.size / 2].to_i
+  right = mark[mark.size / 2..].to_i
+
+  h[k] = if count.zero?
+           1
+         elsif stone.zero?
+           h[[1, count - 1]]
+         elsif mark.size.even?
+           h[[left, count - 1]] + h[[right, count - 1]]
+         else
+           h[[stone * 2024, count - 1]]
+         end
 end
 
+puts(@stones.sum { |stone| @count[[stone, 75]] })
