fc987af62a870d63ba4c8567ca7bc0a2b5a63de5
Tanzeeb Khalili <tanzeeb@gmail.com>
2024-12-13 02:17:12 -0500

    Day 13, b

--- 2024/13/b.rb
+++ 2024/13/b.rb
@@ -1,7 +1,21 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-ARGF.readlines(chomp: true).each do |line|
-  puts line
+@machines = ARGF.readlines(chomp: true).slice_before(/^$/).map do |lines|
+  lines.reject!(&:empty?)
+
+  a, b, p = lines.map { |l| l.scan(/(\d+)[^\d]+(\d+)$/).first.map(&:to_i) }
+
+  [a, b, p.map { |v| v + 10_000_000_000_000 }]
 end
 
+@tokens = @machines.map do |a, b, p|
+  an = (p.first * b.last) - (b.first * p.last)
+  bn = (a.first * p.last) - (p.first * a.last)
+
+  d = (a.first * b.last) - (b.first * a.last)
+
+  (3 * an / d) + (bn / d) if !d.zero? && (an % d).zero? && (bn % d).zero?
+end
+
+pp @tokens.reject(&:nil?).sum
