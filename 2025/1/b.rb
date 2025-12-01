6f7188a989f2815416b4affd6800503b70ef38d9
Tanzeeb Khalili <tanzeeb@gmail.com>
2025-12-01 01:44:15 -0500

    day 1, part 2 wip

--- 2025/1/b.rb
+++ 2025/1/b.rb
@@ -1,7 +1,38 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+current = 50
+count = 0
+
 ARGF.readlines(chomp: true).each do |line|
-  puts line
+  print "#{count} - #{current} - #{line}"
+  case line
+  when /^L(\d+)$/
+    rotation = Regexp.last_match(1).to_i
+    count += rotation / 100
+
+    print " * L#{rotation % 100} - #{rotation / 100} + #{(current - rotation % 100) < 0 ? 1 : 0}" if rotation >= 100
+
+    rotation %= 100
+
+    current = (current - rotation)
+    count += 1 if current < 0
+
+    current %= 100
+  when /^R(\d+)$/
+    rotation = Regexp.last_match(1).to_i
+    count += rotation / 100
+
+    print " * R#{rotation % 100} - #{rotation / 100} + #{(current + rotation % 100) >= 100 ? 1 : 0}" if rotation >= 100
+
+    rotation %= 100
+
+    current = (current + rotation)
+    count += 1 if current >= 100
+
+    current %= 100
+  end
+  print "\n"
 end
 
+puts count
