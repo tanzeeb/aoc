bdbc3da20fa8b012f5441677fd40ea7f0509f4b6
Tanzeeb Khalili <tanzeeb@gmail.com>
2025-12-01 00:44:12 -0500

    day 1, part a

--- 2025/1/a.rb
+++ 2025/1/a.rb
@@ -1,7 +1,18 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+current = 50
+count = 0
+
 ARGF.readlines(chomp: true).each do |line|
-  puts line
+  case line
+  when /^L(\d+)$/
+    current = (current - Regexp.last_match(1).to_i) % 100
+  when /^R(\d+)$/
+    current = (current + Regexp.last_match(1).to_i) % 100
+  end
+
+  count += 1 if current.zero?
 end
 
+puts count
