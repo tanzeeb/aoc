8381f9538f537fbcbc4adbe0d3a61d0b8f57acf6
Tanzeeb Khalili <tanzeeb@gmail.com>
2025-12-03 00:26:07 -0500

    day 3, part 1

--- 2025/3/a.rb
+++ 2025/3/a.rb
@@ -1,7 +1,21 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+sum = 0
+
 ARGF.readlines(chomp: true).each do |line|
-  puts line
+  batteries = line.split(//).map(&:to_i)
+
+  max = 0
+
+  batteries.each_with_index do |a, i|
+    batteries[i + 1..].each do |b,|
+      joltage = a * 10 + b
+      max = joltage if joltage > max
+    end
+  end
+
+  sum += max
 end
 
+puts sum
