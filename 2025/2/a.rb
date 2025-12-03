8380c066e1980bfecf038bcfe1939bf01887739a
Tanzeeb Khalili <tanzeeb@gmail.com>
2025-12-03 01:07:04 -0500

    day 2, part 1

--- 2025/2/a.rb
+++ 2025/2/a.rb
@@ -1,7 +1,17 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-ARGF.readlines(chomp: true).each do |line|
-  puts line
+ranges = ARGF.readlines(chomp: true).join('').split(/,/).map do |range|
+  Range.new(*range.split(/-/).map(&:to_i))
 end
 
+sum = 0
+
+ranges.each do |range|
+  range.each do |n|
+    s = n.to_s
+    sum += n if s[0...(s.size / 2)] == s[(s.size / 2)..]
+  end
+end
+
+puts sum
