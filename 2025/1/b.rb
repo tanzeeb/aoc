ba3b1bff29e32c62b9eb0227fe838b5379a4ef3c
Tanzeeb Khalili <tanzeeb@gmail.com>
2025-12-03 00:01:20 -0500

    day 1, part 2 stil wip

--- 2025/1/b.rb
+++ 2025/1/b.rb
@@ -4,35 +4,35 @@
 current = 50
 count = 0
 
+puts "\t#{current} \t= #{count}"
 ARGF.readlines(chomp: true).each do |line|
-  print "#{count} - #{current} - #{line}"
+  print "#{line}"
+
   case line
   when /^L(\d+)$/
     rotation = Regexp.last_match(1).to_i
-    count += rotation / 100
 
-    print " * L#{rotation % 100} - #{rotation / 100} + #{(current - rotation % 100) < 0 ? 1 : 0}" if rotation >= 100
+    current -= rotation
 
-    rotation %= 100
+    count += (current / 100).abs - 1
+    count += 1 if current.zero?
 
-    current = (current - rotation)
-    count += 1 if current < 0
+    print ' *' if current.zero?
 
     current %= 100
   when /^R(\d+)$/
     rotation = Regexp.last_match(1).to_i
-    count += rotation / 100
 
-    print " * R#{rotation % 100} - #{rotation / 100} + #{(current + rotation % 100) >= 100 ? 1 : 0}" if rotation >= 100
+    current += rotation
 
-    rotation %= 100
+    count += (current / 100).abs
 
-    current = (current + rotation)
-    count += 1 if current >= 100
+    print ' *' if current.zero?
 
     current %= 100
   end
-  print "\n"
+
+  puts "\t#{current} \t= #{count}"
 end
 
 puts count
